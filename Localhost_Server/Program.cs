b3ee8c240baef427058dad28bdf77bb4746a4ac5
Андрей Михайленко <andreimikhailencko@gmail.com>
2024-12-11 23:58:12 +0200

    Спроба виправлення помилок

--- Localhost_Server/Program.cs
+++ Localhost_Server/Program.cs
@@ -20,28 +20,37 @@ class Server
     static void RunServer()
     {
         Console.WriteLine("Сервер запускається...");
-
-        // Завантаження серверного сертифіката
-        var serverCertificate = new X509Certificate2("Certificates/server_certificate.pfx", "Laba3", X509KeyStorageFlags.MachineKeySet);
-
-        TcpListener listener = new TcpListener(IPAddress.Loopback, 50001);
-        listener.Start();
-        Console.WriteLine("Сервер чекає на підключення...");
-
-        while (true)
+        try
         {
-            using var client = listener.AcceptTcpClient();
-            using var sslStream = new SslStream(client.GetStream(), false, ValidateClientCertificate);
+            // Завантаження серверного сертифіката
+            var serverCertificate = new X509Certificate2("Certificates/ServerCertificate.pfx", "ThebestPassword", X509KeyStorageFlags.DefaultKeySet);
 
-            sslStream.AuthenticateAsServer(serverCertificate, true, System.Security.Authentication.SslProtocols.Tls13, false);
-            Console.WriteLine("Клієнт підключився.");
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 5000);
+            listener.Start();
+            Console.WriteLine("Сервер чекає на підключення...");
 
-            // Отримання повідомлення
-            byte[] buffer = new byte[2048];
-            int bytesRead = sslStream.Read(buffer, 0, buffer.Length);
-            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-            Console.WriteLine($"Отримано повідомлення: {message}");
+            while (true)
+            {
+                using var client = listener.AcceptTcpClient();
+                using var sslStream = new SslStream(client.GetStream(), false, ValidateClientCertificate);
+
+                sslStream.AuthenticateAsServer(serverCertificate, true, System.Security.Authentication.SslProtocols.Tls13, false);
+                Console.WriteLine("Клієнт підключився.");
+
+                // Отримання повідомлення
+                byte[] buffer = new byte[2048];
+                int bytesRead = sslStream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Отримано повідомлення: {message}");
+            }
         }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка завантаження сертифіката сервера: {ex.Message}");
+            return;
+        }
+
     }
 
     static bool ValidateClientCertificate(object sender, X509Certificate? certificate, X509Chain? chain, SslPolicyErrors sslPolicyErrors)
